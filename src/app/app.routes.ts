5f7e021015fce354c09f7d73ac816885dfbb2e7f
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2025-05-26 11:12:24 -0300

    Corrige rota raiz com redirecionamento para /login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,21 +4,23 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { UserComponent } from './pages/user/user.component';
 import { AuthGuard } from './services/auth-guard.service';
 
-export const routes: Routes =
-[
+export const routes: Routes = [
   {
-  path: "login",
-  component: LoginComponent
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
-
   {
-    path: "signup",
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
     component: SignupComponent
   },
   {
-    path: "user",
+    path: 'user',
     component: UserComponent,
     canActivate: [AuthGuard]
-    }
-
+  }
 ];
