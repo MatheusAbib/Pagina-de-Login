cbb3f8c4cce2398f62c5c6cb795249b1fffdc7a8
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2026-07-28 08:29:02 -0300

    deploy: tela de usuario após o login

--- login-auth-api/login-auth-api/Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/login-auth-api-0.0.1-SNAPSHOT.jar
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://login-auth-api.onrender.com',
-  useMock: true
-};
